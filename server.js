d3a5486208bdb3df1cbd6e434226b3e7775eec05
Dominik Dumaine <bondifrench@gmail.com>
2015-11-22 08:48:13 +1100

    adding socketid to log

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '/client')));
 app.use(logger('dev'));
 
 app.get('*', function (req, res) {
-	res.sendfile('./index.html')
+	res.sendFile('index.html', {root: __dirname + '/client/'})
 });
 
 app.use(function (err, req, res, next) {
@@ -21,13 +21,14 @@ console.log('Server has started on port: '+ app.get('port'));
 var io = require('socket.io').listen(app.listen(app.get('port')));
 
 io.sockets.on('connection', function (socket) {
-	console.log('A user connected');
+	console.log('A user connected, Socket %id', socket.id);
 	socket.emit('message', {message: 'Welcome to the chat room!'});
 	socket.on('send', function (data) {
+		data["socketid"] = socket.id
 		console.log(data);
 		io.sockets.emit('message', data)
 	});
 	socket.on('disconnect', function () {
-		console.log('user disconnected');
+		console.log('User disconnected. Socket id %s', socket.id);
 	})
 });
